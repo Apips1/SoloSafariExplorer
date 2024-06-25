931b190b25e3f325b425009a850c5b2ea11b7c8e
Afif Nugroho <92508998+Apips1@users.noreply.github.com>
2024-06-25 08:56:33 +0700

    Initial Commit

---
+++ Assets/Script/audio.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio : MonoBehaviour
+{
+    public AudioSource sound;
+    // Start is called before the first frame update
+    public void play()
+    {
+        sound.Play();
+    }
+
+    public void stop()
+    {
+        sound.Stop();
+    }
+}
